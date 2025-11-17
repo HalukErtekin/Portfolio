b6ccd53aca4f5427d06998c3cbb6aa092ff83146
HalukErtekin <halukertekin1907@gmail.com>
2025-11-17 16:05:06 +0300

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,9 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   icons: {
+    icon: "/favicon.ico",
     shortcut: "/favicon.png",
+    apple: "/apple-touch-icon.png",
   },
 };
 const inter = Inter({
